983a81ad069ba418739f61e139fbec35008119ce
zhangyongli <zhangyongli@bjca.org.cn>
2020-05-19 23:35:47 +0800

    2次修改

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,11 +25,75 @@ $(function(){
 
     $('.news-item-box').click(function(){
         var img = $(this).find('.news-item-wrap');
-        alert(document.documentElement.style.fontSize+','+$(this).parent('.news-item').width()+','+img.width()+','+img.height());
+       // alert(document.documentElement.style.fontSize+','+$(this).parent('.news-item').width()+','+img.width()+','+img.height());
     })
+
+    if($('.studio-img-wrap').length>0){
+        var fontSize = parseInt(document.documentElement.style.fontSize);
+        $(".studio-img-wrap .studio-img").mouseenter(function(e){
+            if(e.offsetX/fontSize > 3.375){
+                $(".studio-left-pop").fadeOut();
+                $(".studio-right-pop").fadeIn();
+            }else{
+                $(".studio-right-pop").fadeOut();
+                $(".studio-left-pop").fadeIn();
+            }
+            // console.log('enter',e.offsetX,e.offsetX/fontSize)
+        }).mousemove(function(e){
+            // console.log('move',e.offsetX,e.offsetX/fontSize)
+            if(e.offsetX/fontSize > 3.375){
+                $(".studio-left-pop").fadeOut();
+                $(".studio-right-pop").fadeIn();
+            }else{
+                $(".studio-right-pop").fadeOut();
+                $(".studio-left-pop").fadeIn();
+            }
+        }).mouseout(function(){
+            $(".studio-left-pop").fadeOut();
+            $(".studio-right-pop").fadeOut();
+        })
+    }
+
+    scrollToTop();
+
+    readWorksDetail();
 })
 AOS.init({
 	easing: 'ease',
 	duration: 1000,
 	once: true
 });
+//  scoll to Top
+function scrollToTop() {
+    if ($('.scroll-to-target').length) {
+        $(".scroll-to-target").on('click', function() {
+            var target = $(this).attr('data-target');
+            // animate
+            $('html, body').animate({
+                scrollTop: $(target).offset().top
+            }, 1000);
+
+        });     
+        jQuery(window).on('scroll', function(){
+            var strickyScrollPos = 100;
+            if ($(window).scrollTop() > strickyScrollPos) {         
+                $('.scroll-to-top').fadeIn(1500);
+            } else{           
+                $('.scroll-to-top').fadeOut(1500);
+            }
+        });   
+    }
+}
+
+/***
+ * worksDet
+ */
+function readWorksDetail(){
+    if($("#readMoreDetail").length>0){
+        $("#readMoreDetail").click(function(){
+            $("#worksInfoMore").slideDown();
+            $("#worksInfoFirst").removeClass("first");
+            $(this).hide();
+        })
+    }
+}
