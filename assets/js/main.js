98f833d1c9de814b19c1e6eadb7e1223290b3288
zhangyongli <zhangyongli@bjca.org.cn>
2020-06-22 19:23:39 +0800

    --

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,7 @@ $(function(){
                $stickyHeader.slideUp(300)
             }
         })
+        mobileTouch();
     }
 
     $('.news-item-box').click(function(){
@@ -137,4 +138,77 @@ function readWorksDetail(){
             $(this).hide();
         })
     }
+}
+
+function mobileTouch(){
+    var $stickyHeader = $('#stickyHeader');
+var startx, starty;
+//获得角度
+function getAngle(angx, angy) {
+    return Math.atan2(angy, angx) * 180 / Math.PI;
+};
+
+//根据起点终点返回方向 1向上 2向下 3向左 4向右 0未滑动
+function getDirection(startx, starty, endx, endy) {
+    var angx = endx - startx;
+    var angy = endy - starty;
+    var result = 0;
+
+    //如果滑动距离太短
+    if (Math.abs(angx) < 2 && Math.abs(angy) < 2) {
+        return result;
+    }
+
+    var angle = getAngle(angx, angy);
+    if (angle >= -135 && angle <= -45) {
+        result = 1;
+    } else if (angle > 45 && angle < 135) {
+        result = 2;
+    } else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
+        result = 3;
+    } else if (angle >= -45 && angle <= 45) {
+        result = 4;
+    }
+
+    return result;
+}
+//手指接触屏幕
+document.addEventListener("touchstart", function(e){
+    startx = e.touches[0].pageX;
+    starty = e.touches[0].pageY;
+}, false);
+//手指离开屏幕
+document.addEventListener("touchend", function(e) {
+    var scrollTop = $(window).scrollTop();
+   if(scrollTop<300){
+    $stickyHeader.slideUp(300)
+    return false;
+   }
+
+    var endx, endy;
+    endx = e.changedTouches[0].pageX;
+    endy = e.changedTouches[0].pageY;
+   
+    var direction = getDirection(startx, starty, endx, endy);
+    switch (direction) {
+        case 0:
+            // alert("未滑动！");
+            break;
+        case 1:
+          //  alert("向上！");
+          $stickyHeader.slideUp(300)
+            break;
+        case 2:
+          //  alert("向下！");
+          $stickyHeader.slideDown(300)
+            break;
+        case 3:
+           // alert("向左！");
+            break;
+        case 4:
+          //  alert("向右！");
+            break;
+        default:
+    }
+}, false);
 }
